bf3a13a91fa918c588765b1ae218980d5988cf95
arojaspe <arojaspe@unal.edu.co>
2023-05-08 22:54:43 -0500

    Día 3 haciendo el taller: restricciones PL

--- prueba.py
+++ prueba.py
@@ -5,13 +5,19 @@ from ply import yacc
 tokens = [
     'MIN',      # Token para "min"
     'MAX',      # Token para "max"
+    'FO',       # Token para función
+    'R',        # Token para restricción
     'VAR',      # Token para nombres de variables
     'NUMBER',   # Token para números
     'PLUS',     # Token para el operador "+"
     'MINUS',    # Token para el operador "-"
     'TIMES',    # Token para el operador "*"
     'DIVIDE',   # Token para el operador "/"
-    'EQUAL'     # Token para el operador "="
+    'EQUAL',     # Token para el operador "="
+    'LT',        # Token para el operador "<"
+    'LE',        # Token para el operador "<="
+    'GT',        # Token para el operador ">"
+    'GE'        # Token para el operador ">="
 ]
 
 # Expresiones regulares para tokens simples
@@ -20,6 +26,10 @@ t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'\/'
 t_EQUAL = r'\='
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'>='
 t_ignore = ' \t'
 
 # Expresiones regulares para tokens complejos
@@ -29,6 +39,14 @@ def t_MIN(t):
 
 def t_MAX(t):
     r'max'
+    return t
+
+def t_FO(t):
+    r'fo'
+    return t
+
+def t_R(t):
+    r'r'
     return t
 
 def t_VAR(t):
@@ -49,35 +67,51 @@ def t_error(t):
 lexer = lex.lex()
 
 # Definición de la gramática
+def p_problema(p):
+    '''problema : funcionobjetivo
+                | restriccion'''
+    p[0] = p[1]
+
 def p_funcionobjetivo(p):
-    '''funcionobjetivo : MIN expresion
-                  | MAX expresion'''
-    p[0] = (('Palabra reservada',p[1]), ('Expresión ', p[2]))
+    '''funcionobjetivo : FO MIN expresion
+                       | FO MAX expresion'''
+    p[0] = ('funcionobjetivo', ('Palabra reservada', p[2]), p[3])
+
+def p_restriccion(p):
+    '''restriccion : R expresion
+                   | R expresion LT expresion   
+                   | R expresion LE expresion
+                   | R expresion GT expresion
+                   | R expresion GE expresion
+                   | R expresion EQUAL expresion'''
+    if len(p) == 2:
+        p[0] = ('restriccion', p[2])
+    else:
+        p[0] = ('restriccion', p[2], ('operador de comparacion', p[3]), p[4])
 
 def p_expresion(p):
     '''expresion : termino
-            | termino PLUS expresion
-            | termino MINUS expresion
-            | termino EQUAL expresion
-            | termino DIVIDE expresion'''
-    
+                 | expresion PLUS termino
+                 | expresion MINUS termino
+                 | expresion EQUAL termino
+                 | expresion DIVIDE termino'''
     if len(p) == 2:
-        p[0] = p[1]
+        p[0] = ('expresion', p[1])
     else:
-        p[0] = (('término ',p[1]), ('operador',p[2]), (p[3]))
+        p[0] = ('expresion', p[1], ('operador', p[2]), p[3])
 
 def p_termino(p):
     '''termino : factor
-                | factor TIMES termino'''
+               | termino TIMES factor'''
     if len(p) == 2:
-        p[0] = ('variable',p[1])
+        p[0] = ('termino', p[1])
     else:
-        p[0] = (('número',p[1]), ('operador',p[2]), p[3])
+        p[0] = ('termino', p[1], ('operador', p[2]), ('factor', p[3]))
 
 def p_factor(p):
     '''factor : NUMBER
               | VAR'''
-    p[0] = p[1]
+    p[0] = ('numero', p[1])
 
 def p_error(p):
     print("Error de sintaxis en '%s'" % p.value)
@@ -95,7 +129,7 @@ def main():
         if not s:
             continue
         result = parser.parse(s)
-        print(result, "\n")
+        #print(result, "\n")
         contador = 0
         for elem in (str(result).split('(')):
             elem = elem.strip().replace(')', "")
@@ -107,5 +141,6 @@ def main():
                     contador-=1 
             else:
                 contador+=1
+                
 if __name__ == '__main__':
     main()
